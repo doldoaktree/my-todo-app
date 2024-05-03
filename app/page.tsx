46c56a3b4f4d015ee3e7f2415a7e9e1d3a1c6a91
doldoaktree <doldoaktree@gmail.com>
2024-05-03 15:08:10 +1000

    updated dependencies and added like-button.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button";
+import LikeButton from "./like-button";
 
 export default function Home() {
+
   return (
     <main>
       <form className="flex-auto p-6">
@@ -8,7 +9,7 @@ export default function Home() {
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
        
           <h1 className="flex-auto font-medium text-slate-900">Learn Shaddy</h1>
-          <Button variant="secondary">Click me</Button>
+         <LikeButton/>
         </div>
       </form>
     </main>
