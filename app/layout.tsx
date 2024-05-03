eab9e0aaed215842c6138ecfd7b677b16f7f6a6c
doldoaktree <doldoaktree@gmail.com>
2024-05-03 13:45:52 +1000

    fixed error, rendering properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "@/styles/globals.css";
-import { cn } from "@/lib/utils";
 
 
 export const fontSans = FontSans({
@@ -21,15 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
-      <body className="text-3xl font-bold underline"
-        // className={cn(
-        //   "min-h-screen bg-background font-sans antialiased",
-        //   fontSans.variable
-        // )}
-      ><div>Hello</div>
-        {/* {children} */}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
-// import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Learn Shaddy</h1>
-      <Button variant="secondary">Click me</Button>
+      <form className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+       
+          <h1 className="flex-auto font-medium text-slate-900">Learn Shaddy</h1>
+          <Button variant="secondary">Click me</Button>
+        </div>
+      </form>
     </main>
   );
 }
